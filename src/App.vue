db03849d1385e4f3d8401505609ccce921fca5a3
CarlosOrtiz-1979 <diortiz72@gmail.com>
2025-03-23 19:39:51 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout class="rounded rounded-md border">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list nav>
+        <v-list-item title="About" link to="/about"></v-list-item>
+        <v-list-item title="Ejemplo" link to="/example"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" height="300">
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
